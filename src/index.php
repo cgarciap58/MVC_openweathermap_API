efd1fdc9464b2a26baf6f73283184855560788bc
César García <cgarciap58@iesalbarregas.es>
2026-03-20 00:34:29 +0100

    index simplificado

--- src/index.php
+++ src/index.php
@@ -1,19 +1,5 @@
 <?php
 
-declare(strict_types=1);
+require_once __DIR__ . '/Views/index.php';
 
-require_once __DIR__ . '/Controllers/weather_controller.php';
-
-$hasQuery = isset($_GET['city']) || isset($_GET['view']) || isset($_GET['type']);
-
-if ($hasQuery) {
-    $controller = new WeatherController();
-    $controller->handleRequest($_GET);
-    return;
-}
-
-View::show('weather_search', [
-    'title' => 'Buscar ciudad',
-    'selected_type' => 'current',
-    'city' => '',
-]);
+?>
